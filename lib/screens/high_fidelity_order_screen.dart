271f6c9233e9216343e478e1e913f3a2f046ef82
mahmoodBarish <45651152+mahmoodBarish@users.noreply.github.com>
2025-10-07 16:23:44 +0300

    Update high_fidelity_order_screen.dart

--- lib/screens/high_fidelity_order_screen.dart
+++ lib/screens/high_fidelity_order_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package.google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HighFidelityOrderScreen extends StatefulWidget {
   const HighFidelityOrderScreen({super.key});
